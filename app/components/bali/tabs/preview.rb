3683ba9ccffebfde6cee4c6fce95c72e3c37d2d0
Federico Gonzalez <fedegl@hey.com>
2022-04-19 13:43:27 -0700

    Fix Tabs::Preview

--- app/components/bali/tabs/preview.rb
+++ app/components/bali/tabs/preview.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
-class Tabs::Preview < ApplicationViewComponentPreview
-  # You can specify the container class for the default template
-  # self.container_class = "w-1/2 border border-gray-300"
-
+class Bali::Tabs::Preview < ApplicationViewComponentPreview
   def default
   end
 end
