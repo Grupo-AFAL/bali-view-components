9bf95b31947e175e38ff7c1793943acf31e50789
Federico Gonzalez <fedegl@hey.com>
2022-04-25 16:24:38 -0700

    Customize image class

--- app/components/bali/image_grid/image/component.rb
+++ app/components/bali/image_grid/image/component.rb
@@ -5,6 +5,12 @@ module Bali
     module Image
       class Component < ApplicationViewComponent
         renders_one :footer
+
+        attr_reader :image_class
+
+        def initialize(image_class: 'is-3by2')
+          @image_class = image_class
+        end
       end
     end
   end
