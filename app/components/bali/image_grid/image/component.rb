abc1fa9df05d94748003e86ca68a379950398f85
Federico Gonzalez <fedegl@hey.com>
2022-04-25 17:29:18 -0700

    Allow passing options to the image and its footer

--- app/components/bali/image_grid/image/component.rb
+++ app/components/bali/image_grid/image/component.rb
@@ -4,12 +4,23 @@ module Bali
   module ImageGrid
     module Image
       class Component < ApplicationViewComponent
-        renders_one :footer
+        renders_one :footer, 'Bali::ImageGrid::Image::FooterComponent'
 
         attr_reader :image_class
 
-        def initialize(image_class: 'is-3by2')
+        def initialize(image_class: 'is-3by2', **options)
           @image_class = image_class
+          @options = prepend_class_name(options, 'column is-one-quarter')
+        end
+      end
+
+      class FooterComponent < ApplicationViewComponent
+        def initialize(**options)
+          @options = prepend_class_name(options, 'card-footer')
+        end
+
+        def call
+          tag.footer(**@options) { content }
         end
       end
     end
