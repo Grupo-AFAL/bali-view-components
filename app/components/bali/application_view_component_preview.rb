a1672b9421ae7e82400ef941b5eb50d1dfd57287
alexisKinto <mgutierrez@afal.mx>
2022-05-06 16:42:48 -0700

    update aplication view component preview

--- app/components/bali/application_view_component_preview.rb
+++ app/components/bali/application_view_component_preview.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
-class ApplicationViewComponentPreview < ViewComponentContrib::Preview::Base
-  self.abstract_class = true
+module Bali
+  class ApplicationViewComponentPreview < ViewComponentContrib::Preview::Base
+    self.abstract_class = true
+  end
 end
