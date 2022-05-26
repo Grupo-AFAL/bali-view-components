6d6604cf0ce5a6e4de7026bfe683d6a59586b0e7
Sergio Lopez <sergio@softnya.com>
2022-05-26 10:04:33 -0500

    Vista previa de componente card

--- app/components/bali/card/preview.rb
+++ app/components/bali/card/preview.rb
@@ -1,6 +1,19 @@
 # frozen_string_literal: true
 
-class Bali::Card::Preview < ApplicationViewComponentPreview
-  def default
+module Bali
+  module Card
+    class Preview < ApplicationViewComponentPreview
+
+      def default
+        render Card::Component.new(title: 'Title', description: 'Description', image: 'https://via.placeholder.com/320x244.png', link: '#') do |c|
+          c.media do
+            tag.div 'Media'
+          end
+          c.footer_item do
+            tag.a 'Footer item with link', href: '#'
+          end
+        end
+      end
+    end
   end
 end
