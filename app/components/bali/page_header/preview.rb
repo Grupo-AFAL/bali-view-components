21950f0c58242d7e7208f42114c8924b3380859a
Miguel Frias <migue_tj_frias@hotmail.com>
2022-06-01 08:23:02 -0700

    Make page header more flexible

--- app/components/bali/page_header/preview.rb
+++ app/components/bali/page_header/preview.rb
@@ -4,11 +4,36 @@ module Bali
   module PageHeader
     class Preview < ApplicationViewComponentPreview
       def default
+        render PageHeader::Component.new(title: 'Title') do |c|
+          tag.a 'Right action', class: 'button is-secondary', href: '#'
+        end
+      end
+
+      def without_right_content
+        render PageHeader::Component.new(title: 'Title')
+      end
+
+      def with_subtitle_as_param
+        render PageHeader::Component.new(title: 'Title', subtitle: 'Subtitle') do |c|
+          tag.a 'Right action', class: 'button is-secondary', href: '#'
+        end
+      end
+
+      def with_title_and_subtitle_as_slots
         render PageHeader::Component.new do |c|
-          c.title('Title')
-          c.right_panel do
-            tag.a 'Right action', class: 'button is-secondary', href: '#'
-          end
+          c.title('Title', class: 'title is-3')
+          c.subtitle('Subtitle', class: 'subtitle is-6')
+
+          tag.a 'Right action', class: 'button is-secondary', href: '#'
+        end
+      end
+
+      def with_title_and_subtitle_as_block
+        render PageHeader::Component.new do |c|
+          c.title { tag.h1('Title', class: 'title is-3') }
+          c.subtitle { tag.h1('Subtitle', class: 'subtitle is-6') }
+
+          tag.a 'Right action', class: 'button is-secondary', href: '#'
         end
       end
     end
--- spec/components/bali/page_header_spec.rb
+++ spec/components/bali/page_header_spec.rb
@@ -3,44 +3,51 @@
 require 'rails_helper'
 
 RSpec.describe Bali::PageHeader::Component, type: :component do
-  let(:component) { Bali::PageHeader::Component.new }
+  let(:component) { Bali::PageHeader::Component.new(**@options) }
+
+  before { @options = {} }
 
   subject { rendered_component }
 
   describe 'rendering' do
-    context 'title' do
-      it 'renders' do
-        render_inline(component) do |c|
-          c.title('Page Header')
-        end
+    context 'with title and subtitle as params' do
+      before { @options.merge!(title: 'Title', subtitle: 'Subtitle') }
 
-        is_expected.to have_css '.level-left h1.title', text: 'Page Header'
+      it 'renders' do
+        render_inline(component)
+
+        is_expected.to have_css '.level-left h1.title', text: 'Title'
+        is_expected.to have_css '.level-left p.subtitle', text: 'Subtitle'
       end
     end
 
-    context 'subtitle' do
-      it 'renders' do
-        render_inline(component) do |c|
-          c.title('Page Header')
-          c.subtitle('Header subtitle')
+    context 'with title and subtitle as slots' do
+      context 'when using text param' do
+        it 'renders' do
+          render_inline(component) do |c|
+            c.title('Title')
+            c.subtitle('Subtitle')
+            'Right content'
+          end
+  
+          is_expected.to have_css '.level-left h1.title', text: 'Title'
+          is_expected.to have_css '.level-left p.subtitle', text: 'Subtitle'
+          is_expected.to have_css '.level-right', text: 'Right content'
         end
-
-        is_expected.to have_css '.level-left h1.title', text: 'Page Header'
-        is_expected.to have_css '.level-left p.subtitle', text: 'Header subtitle'
       end
-    end
 
-    context 'with contents within level-right' do
-      it 'renders' do
-        render_inline(component) do |c|
-          c.title('Page Header')
-          c.subtitle('Header subtitle')
-          c.right_panel { 'Right content' }
+      context 'when using blocks' do
+        it 'renders' do
+          render_inline(component) do |c|
+            c.title { '<h2 class="title">Title</h2>'.html_safe }
+            c.subtitle { '<p class="subtitle">Subtitle</p>'.html_safe }
+            'Right content'
+          end
+  
+          is_expected.to have_css '.level-left h2.title', text: 'Title'
+          is_expected.to have_css '.level-left p.subtitle', text: 'Subtitle'
+          is_expected.to have_css '.level-right', text: 'Right content'
         end
-
-        is_expected.to have_css '.level-left h1.title', text: 'Page Header'
-        is_expected.to have_css '.level-left p.subtitle', text: 'Header subtitle'
-        is_expected.to have_css '.level-right', text: 'Right content'
       end
     end
   end
