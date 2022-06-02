75969db35a8f3944ca2e83ecc0bd2f452cd12283
Miguel Frias <migue_tj_frias@hotmail.com>
2022-06-02 15:37:18 -0700

    Add tests

--- spec/components/bali/chart_spec.rb
+++ spec/components/bali/chart_spec.rb
@@ -3,14 +3,18 @@
 require 'rails_helper'
 
 RSpec.describe Bali::Chart::Component, type: :component do
-  let(:options) { {} }
-  let(:component) { Bali::Chart::Component.new(**options) }
+  let(:component) { Bali::Chart::Component.new(**@options) }
+
+  before { @options = {} }
 
   subject { rendered_component }
 
   it 'renders' do
+    @options.merge!(data: { chocolate: 3 }, title: 'Chocolate Sales', id: 'chocolate-sales')
     render_inline(component)
 
     is_expected.to have_css 'div'
+    is_expected.to have_css 'h3.title', text: 'Chocolate Sales'
+    is_expected.to have_css 'canvas'
   end
 end
