3540ef26803510daed2f6244c1989b1da76665a1
Miguel Frias <migue_tj_frias@hotmail.com>
2022-06-03 07:42:50 -0700

    Add tests

--- spec/components/bali/chart_spec.rb
+++ spec/components/bali/chart_spec.rb
@@ -5,16 +5,56 @@ require 'rails_helper'
 RSpec.describe Bali::Chart::Component, type: :component do
   let(:component) { Bali::Chart::Component.new(**@options) }
 
-  before { @options = {} }
+  before { @options = { data: { chocolate: 3 } } }
 
   subject { rendered_component }
 
-  it 'renders' do
-    @options.merge!(data: { chocolate: 3 }, title: 'Chocolate Sales', id: 'chocolate-sales')
+  it 'renders a chart title' do
+    @options.merge!(title: 'Chocolate Sales', id: 'chocolate-sales')
     render_inline(component)
 
-    is_expected.to have_css 'div'
     is_expected.to have_css 'h3.title', text: 'Chocolate Sales'
-    is_expected.to have_css 'canvas'
+  end
+
+  it 'renders a div with chart controller' do
+    render_inline(component)
+
+    expect(rendered_component).to have_css 'canvas.chart'
+    expect(rendered_component).to have_css 'canvas[data-controller="chart"]'
+  end
+
+  it 'renders a line chart' do
+    @options.merge!(type: :line)
+    render_inline(component)
+
+    expect(rendered_component).to have_css 'canvas[data-chart-type-value="line"]'
+  end
+
+  it 'renders a pie chart' do
+    @options.merge!(type: :pie)
+    render_inline(component)
+
+    expect(rendered_component).to have_css 'canvas[data-chart-type-value="pie"]'
+  end
+
+  it 'renders a bar chart' do
+    @options.merge!(type: :bar)
+    render_inline(component)
+
+    expect(rendered_component).to have_css 'canvas[data-chart-type-value="bar"]'
+  end
+
+  it 'renders a doughnut chart' do
+    @options.merge!(type: :doughnut)
+    render_inline(component)
+
+    expect(rendered_component).to have_css 'canvas[data-chart-type-value="doughnut"]'
+  end
+
+  it 'renders a polarArea chart' do
+    @options.merge!(type: :polarArea)
+    render_inline(component)
+
+    expect(rendered_component).to have_css 'canvas[data-chart-type-value="polarArea"]'
   end
 end
