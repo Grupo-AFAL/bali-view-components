da97578781014df04945c6b4bf2cd45e04448d80
Miguel Frias <migue_tj_frias@hotmail.com>
2022-06-03 15:23:49 -0700

    Add tests for icon component

--- spec/components/bali/icon_spec.rb
+++ spec/components/bali/icon_spec.rb
@@ -3,14 +3,23 @@
 require 'rails_helper'
 
 RSpec.describe Bali::Icon::Component, type: :component do
-  let(:options) { {} }
-  let(:component) { Bali::Icon::Component.new(**options) }
+  let(:component) { Bali::Icon::Component.new('snowflake',**@options) }
+
+  before { @options = {} }
 
   subject { rendered_component }
 
   it 'renders' do
     render_inline(component)
 
-    is_expected.to have_css 'div'
+    is_expected.to have_css 'span.icon'
+  end
+
+  it 'renders with an id and additional classes' do
+    @options.merge!(id: 'my-icon', class: 'has-text-info')
+    render_inline(component)
+
+    is_expected.to have_css 'span.icon.has-text-info'
+    is_expected.to have_css 'span[id="my-icon"]'
   end
 end
