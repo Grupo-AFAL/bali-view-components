be7c97de6976d3e121a29da932d93b4cb5c64ff4
Miguel Frias <migue_tj_frias@hotmail.com>
2022-06-03 15:38:20 -0700

    Add tests for filters component

--- spec/components/bali/filters_spec.rb
+++ spec/components/bali/filters_spec.rb
@@ -2,15 +2,56 @@
 
 require 'rails_helper'
 
+class DummyFilterForm
+  include ActiveModel::Model
+  include ActiveModel::Attributes
+
+  attribute :name, :string
+
+  def model_name
+    @model_name ||= ActiveModel::Name.new(self, nil, 'q')
+  end
+
+  def id
+    @id ||= 1
+  end
+
+  def result(options = {})
+    @result ||= []
+  end
+
+  def query_params
+    { }
+  end
+
+  def status_in
+  end
+end
+
+
 RSpec.describe Bali::Filters::Component, type: :component do
-  let(:options) { {} }
-  let(:component) { Bali::Filters::Component.new(**options) }
+  let(:component) do
+    Bali::Filters::Component.new(form: DummyFilterForm.new, url: '#', text_field: :name)
+  end
 
   subject { rendered_component }
 
   it 'renders' do
     render_inline(component)
 
-    is_expected.to have_css 'div'
+    is_expected.to have_css 'div.search-input-component'
+    is_expected.not_to have_css 'a.filters-button'
+  end
+
+  it 'renders with filters' do
+    render_inline(component) do |c|
+      c.attribute(
+        title: 'Active',
+        attribute: :status_in,
+        collection_options:  [['active', :true], ['inactive', false]])
+    end
+
+    is_expected.to have_css 'div.search-input-component'
+    is_expected.to have_css 'a[id="filters-button"]'
   end
 end
