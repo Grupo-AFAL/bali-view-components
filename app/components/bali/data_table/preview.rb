5349af7cbad7a4116fddf142055cf1c80816b1cc
Miguel Frias <31300198+MiguelFrias97@users.noreply.github.com>
2022-06-07 11:52:14 -0700

    Add data table (#51)
    * Add filters component
    
    * Fix rubocop
    
    * Add controllers
    
    * Fix standard
    
    * Add submit on change controller
    
    * Add SearchInput
    
    * Add Search input
    
    * Ad icon component
    
    * Add previews for filters and sarch input
    
    * Fix rubocop
    
    * Add tests for icon component
    
    * Add tests for search input
    
    * Solve comment
    
    * Add tests for filters component
    
    * Fix rubocop
    
    * Fix rubocop
    
    * Fix standard
    
    * Update README components list
    
    * Add data table component
    
    * Fix rubocop
    
    * Upgrade read me
    
    * Solve comments

---
+++ app/components/bali/data_table/preview.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module Bali
+  module DataTable
+    class Preview < ApplicationViewComponentPreview
+      FORM = DummyFilterForm.new
+
+      HEADERS = [
+        { name: 'Name' },
+        { name: 'Amount' }
+      ].freeze
+
+      RECORDS = [
+        { name: 'Name 1', amount: 1 },
+        { name: 'Name 2', amount: 2 },
+        { name: 'Name 3', amount: 3 }
+      ].freeze
+
+      def default
+        render_with_template(
+          template: 'bali/data_table/previews/default',
+          locals: {
+            headers: HEADERS,
+            records: RECORDS,
+            form: FORM,
+          }
+        )
+      end
+
+      def with_summary
+        render_with_template(
+          template: 'bali/data_table/previews/default',
+          locals: {
+            headers: HEADERS,
+            records: RECORDS,
+            form: FORM,
+            summary: 'This is a summary'
+          }
+        )
+      end
+    end
+  end
+end
