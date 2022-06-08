9b4d84ae5a2509a9b857a3cdd3926eb7211d7704
Miguel Frias <migue_tj_frias@hotmail.com>
2022-06-08 13:52:43 -0700

    Fix rubocop

--- app/components/bali/modal/component.rb
+++ app/components/bali/modal/component.rb
@@ -6,8 +6,8 @@ module Bali
       def initialize(active: true, **options)
         @active = active
         @wrapper_class = options.delete(:wrapper_class)
-        @options = prepend_class_name(options,'is-active') if @active
-        @options = prepend_class_name(options,"modal-component modal")
+        @options = prepend_class_name(options, 'is-active') if @active
+        @options = prepend_class_name(options, 'modal-component modal')
       end
     end
   end
