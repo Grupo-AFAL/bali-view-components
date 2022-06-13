7aaffd4de11dcb203f3e5e541e3dd45da73f1670
Miguel Frias <migue_tj_frias@hotmail.com>
2022-06-13 14:49:11 -0700

    Add another preview to navbar

--- app/components/bali/navbar/preview.rb
+++ app/components/bali/navbar/preview.rb
@@ -14,6 +14,20 @@ module Bali
           locals: { fullscreen: fullscreen, transparency: transparency }
         )
       end
+
+      # @param fullscreen toggle
+      # @param transparency toggle
+      # @param split_menu_on_mobile toggle
+      def split_menu_on_mobile(fullscreen: false, transparency: false, split_menu_on_mobile: true)
+        render_with_template(
+          template: 'bali/navbar/previews/split_menu_on_mobile',
+          locals: { 
+            fullscreen: fullscreen, 
+            transparency: transparency,
+            split_menu_on_mobile: split_menu_on_mobile
+          }
+        )
+      end
     end
   end
 end
