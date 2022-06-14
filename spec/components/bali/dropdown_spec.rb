d32236097152465be18096b454407d868de3256e
Sergio Lopez <sergio@softnya.com>
2022-06-14 16:02:45 -0500

    Bali dropdown test (#96)
    * Test del dropdown
    
    * Cambio de marca del check en dropdown
    
    * Apliación de pruebas
    
    * Sugerencias rubocop

--- spec/components/bali/dropdown_spec.rb
+++ spec/components/bali/dropdown_spec.rb
@@ -5,12 +5,23 @@ require 'rails_helper'
 RSpec.describe Bali::Dropdown::Component, type: :component do
   let(:options) { {} }
   let(:component) { Bali::Dropdown::Component.new(**options) }
+  before do
+    render_inline(component) do |c|
+      c.trigger(class: 'button') { 'Trigger' }
+
+      c.tag.ul do
+        c.tag.li('Item', class: 'dropdown-item')
+      end
+    end
+  end
 
   subject { rendered_component }
 
-  it 'renders' do
-    render_inline(component)
+  it 'renders dropdown with options' do
+    expect(subject).to have_css '.dropdown-item'
+  end
 
-    expect(subject).to have_css 'div'
+  it 'renders dropdown and check trigger' do
+    expect(subject).to have_css '.button', text: 'Trigger'
   end
 end
