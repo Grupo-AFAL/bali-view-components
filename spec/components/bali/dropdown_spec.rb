f1081086f59bb3a5140a6de094ea7c908320897f
Sergio Lopez <sergio@softnya.com>
2022-06-14 15:55:00 -0500

    Sugerencias rubocop

--- spec/components/bali/dropdown_spec.rb
+++ spec/components/bali/dropdown_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe Bali::Dropdown::Component, type: :component do
   let(:options) { {} }
   let(:component) { Bali::Dropdown::Component.new(**options) }
-  before {
+  before do
     render_inline(component) do |c|
       c.trigger(class: 'button') { 'Trigger' }
 
@@ -13,14 +13,14 @@ RSpec.describe Bali::Dropdown::Component, type: :component do
         c.tag.li('Item', class: 'dropdown-item')
       end
     end
-  }
+  end
 
   subject { rendered_component }
 
   it 'renders dropdown with options' do
     expect(subject).to have_css '.dropdown-item'
   end
-  
+
   it 'renders dropdown and check trigger' do
     expect(subject).to have_css '.button', text: 'Trigger'
   end
