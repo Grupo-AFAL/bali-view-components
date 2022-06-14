a76238232748457fb0501a25b899ed5b3e5b70e2
Sergio Lopez <sergio@softnya.com>
2022-06-14 15:52:55 -0500

    Apliación de pruebas

--- spec/components/bali/dropdown_spec.rb
+++ spec/components/bali/dropdown_spec.rb
@@ -5,18 +5,23 @@ require 'rails_helper'
 RSpec.describe Bali::Dropdown::Component, type: :component do
   let(:options) { {} }
   let(:component) { Bali::Dropdown::Component.new(**options) }
-
-  subject { rendered_component }
-
-  it 'renders dropdown with options' do
+  before {
     render_inline(component) do |c|
       c.trigger(class: 'button') { 'Trigger' }
 
       c.tag.ul do
-        c.tag.li('Item')
+        c.tag.li('Item', class: 'dropdown-item')
       end
     end
+  }
 
-    expect(subject).to have_css 'div'
+  subject { rendered_component }
+
+  it 'renders dropdown with options' do
+    expect(subject).to have_css '.dropdown-item'
+  end
+  
+  it 'renders dropdown and check trigger' do
+    expect(subject).to have_css '.button', text: 'Trigger'
   end
 end
