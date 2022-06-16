6a79525f159d38b4a976b90edcfcfe84531ab48a
Ivan Aguilar <ivan96318@gmail.com>
2022-06-15 17:41:47 -0700

    delete unnecessary space

--- app/components/bali/link/component.rb
+++ app/components/bali/link/component.rb
@@ -7,15 +7,15 @@ module Bali
 
       renders_one :icon, ->(name, **options) { Icon::Component.new(name, **options) }
 
-      #  @param name [String] The name of the link.
-      #  @param href [String] The href of the link.
-      #  @param type [Symbol|String] This adds a class for the link: :primary, :secondary,
+      # @param name [String] The name of the link.
+      # @param href [String] The href of the link.
+      # @param type [Symbol|String] This adds a class for the link: :primary, :secondary,
       #  :success, :danger, :warning also adds the button class.
-      #  @param modal [Boolean] Link to open a modal.
-      #  @param drawer [Boolean] Link to open a drawer.
-      #  @param active_path [String] The path of the active page.
-      #  @param match [Symbol] To check if the path is exact or cotains the path.
-      #  @param method [Symbol|String] Adds a turbo method to the link.
+      # @param modal [Boolean] Link to open a modal.
+      # @param drawer [Boolean] Link to open a drawer.
+      # @param active_path [String] The path of the active page.
+      # @param match [Symbol] To check if the path is exact or cotains the path.
+      # @param method [Symbol|String] Adds a turbo method to the link.
 
       # rubocop:disable Metrics/ParameterLists
       def initialize(href:,
