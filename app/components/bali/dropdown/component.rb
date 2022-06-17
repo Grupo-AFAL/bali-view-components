f606fad586422ce3a4c8cf80d8c478c961bfbf15
Sergio Lopez <sergio@softnya.com>
2022-06-17 14:43:17 -0500

    Agregar caracteristicas adicionales dropdown (#114)
    * Agregar caracteristicas adicionales
    
    * Parametros ajustados en lookbook
    
    * Modulo para alignment_class
    
    Co-authored-by: Miguel Frias  <31300198+MiguelFrias97@users.noreply.github.com>
    
    * Parametro align
    
    Co-authored-by: Miguel Frias  <31300198+MiguelFrias97@users.noreply.github.com>
    
    * Correcciones de modulos
    
    * Sugerencias rubocop
    
    Co-authored-by: Miguel Frias  <31300198+MiguelFrias97@users.noreply.github.com>

--- app/components/bali/dropdown/component.rb
+++ app/components/bali/dropdown/component.rb
@@ -6,11 +6,24 @@ module Bali
       renders_one :trigger, Trigger::Component
       renders_many :items, Item::Component
 
-      def initialize(hoverable: false, close_on_click: true, **options)
+      def alignment_class
+        case @align
+        when :right
+          'is-right'
+        when :center
+          'is-centered'
+        else
+          'is-left'
+        end
+      end
+
+      def initialize(hoverable: false, close_on_click: true, align: :right, **options)
         @hoverable = hoverable
         @close_on_click = close_on_click
         @options = options
+        @align = align
 
+        @options = prepend_class_name(@options, alignment_class)
         @options = prepend_class_name(@options, 'dropdown-component dropdown')
         @options = prepend_controller(@options, 'dropdown')
       end
