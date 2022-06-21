3406aba581d92285ce747a987939d5fea3fe7b88
alexisKinto <mgutierrez@afal.mx>
2022-06-21 10:26:41 -0700

    require active_model/railtie in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,9 @@
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
 ENV['RAILS_ENV'] = 'test'
+
 require_relative '../spec/dummy/config/environment'
+require 'active_model/railtie'
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
