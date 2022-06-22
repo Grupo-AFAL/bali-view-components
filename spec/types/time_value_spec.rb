5034d0278235f5f7d891ea8db230cc9ff6c1e85f
alexisKinto <mgutierrez@afal.mx>
2022-06-22 15:36:01 -0700

    include bali types

--- spec/types/time_value_spec.rb
+++ spec/types/time_value_spec.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-# rubocop:disable Style/MixinUsage
-include Bali::Types
-# rubocop:enable Style/MixinUsage
-
 RSpec.describe Bali::Types::TimeValue do
   describe 'integration with a model' do
     it 'casts a time value from integer to a date string' do
