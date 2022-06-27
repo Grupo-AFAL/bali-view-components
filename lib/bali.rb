8de14e10e921f627c90c6acf9aa33c29f4286578
Alexis GK <mgutierrez@afal.mx>
2022-06-27 09:27:22 -0700

    Add utils methods (#128)
    * add utils methods
    
    * update changelog and bali version

--- lib/bali.rb
+++ lib/bali.rb
@@ -5,6 +5,7 @@ require 'bali/engine'
 require 'bali/filter_form'
 require 'bali/layout_concern'
 require 'bali/types/time_value'
+require 'bali/utils'
 
 module Bali
   # Your code goes here...
---
+++ lib/bali/utils.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+module Bali
+  module Utils
+    def class_names(names, conditional_names = {})
+      if names.is_a?(Hash)
+        conditional_names = names
+        classes = []
+      else
+        classes = Array(names)
+      end
+
+      conditional_names.each do |key, condition|
+        classes.push(key) if condition
+      end
+      classes.join(' ').strip
+    end
+
+    def custom_dom_id(model)
+      "#{model.class.name.tableize.singularize.gsub('/', '_')}_#{model.id}"
+    end
+
+    def test_id_attr(params)
+      return unless params
+
+      test_id = case params
+                when ActiveRecord::Base
+                  custom_dom_id(params)
+                when String
+                  params
+                end
+
+      "test-id=\"#{test_id}\"".html_safe
+    end
+
+    # The last module parent is always "Object", which means the one before to last
+    # is the actual module/namespace we want. Like "INV", "Cafeteria" or "EK"
+    def app_module
+      controller.class.module_parents[-2]
+    end
+  end
+end
