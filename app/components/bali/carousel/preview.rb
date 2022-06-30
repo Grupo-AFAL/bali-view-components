bc12392aac97eb18367eb211fb2be818518bd40d
Ivan Aguilar <ivan96318@gmail.com>
2022-06-30 12:17:11 -0700

    add missing css carousel component

--- app/components/bali/carousel/preview.rb
+++ app/components/bali/carousel/preview.rb
@@ -1,8 +1,10 @@
 module Bali
   module Carousel
     class Preview < ApplicationViewComponentPreview
+      include ActionView::Helpers::AssetUrlHelper
+
       def default
-        render(Carousel::Component.new)
+        render(Carousel::Component.new(images: ['https://via.placeholder.com/320x244.png']))
       end
     end
   end
