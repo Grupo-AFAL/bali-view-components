51c74fdc2370725c59727b3dfed0318f98d7353f
Ivan Aguilar <ivan96318@gmail.com>
2022-07-01 15:31:00 -0700

    add documentation to carousel component

--- app/components/bali/carousel/component.rb
+++ app/components/bali/carousel/component.rb
@@ -6,7 +6,8 @@ module Bali
       renders_many :images
 
       def initialize(selected_image: 0, **options)
-        @options = prepend_class_name(options, 'carousel-component glide')
+        @options = options
+        @options = prepend_class_name(@options, 'carousel-component glide')
         @options = prepend_controller(@options, 'carousel')
         @options = prepend_data_attribute(@options, 'carousel-index-value', selected_image)
       end
--- app/components/bali/carousel/preview.rb
+++ app/components/bali/carousel/preview.rb
@@ -1,10 +1,26 @@
 module Bali
   module Carousel
     class Preview < ApplicationViewComponentPreview
-      include ActionView::Helpers::AssetUrlHelper
-
+      # Carousel
+      # -------------
+      # This will render the basic carousel
       def default
         render(Carousel::Component.new) do |c|
+          c.image do
+            image_tag('https://via.placeholder.com/320x244.png', class: 'image-center')
+          end
+        end
+      end
+
+      # Carousel with options
+      # -------------
+      # This will render the carousel as if it were a slider
+      def with_options
+        render(Carousel::Component.new(data: { 'carousel-type-value': 'slider' })) do |c|
+          c.image do
+            image_tag('https://via.placeholder.com/320x244.png', class: 'image-center')
+          end
+
           c.image do
             image_tag('https://via.placeholder.com/320x244.png', class: 'image-center')
           end
