5d6d34ec97155c9914c0195fae66b354e6a174c0
Federico Gonzalez <fedegl@hey.com>
2022-07-01 10:53:08 -0700

    Add more tests

--- spec/bali/components/breadcrumb_spec.rb
+++ spec/bali/components/breadcrumb_spec.rb
@@ -20,4 +20,22 @@ RSpec.describe Bali::Breadcrumb::Component, type: :component do
     expect(subject).to have_css 'li a[href="/home/section"]', text: 'Section'
     expect(subject).to have_css 'li.is-active a[href="/home/section/page"]', text: 'Page'
   end
+
+  it 'renders breadcrumb with icons' do
+    render_inline(component) do |c|
+      c.item(href: '/home', name: 'Home', icon_name: 'home')
+    end
+
+    expect(subject).to have_css 'li a span.icon'
+    expect(subject).to have_css 'li a span', text: 'Home'
+  end
+
+  it 'renders breadcrumb with custom classes' do
+    options[:class] = 'is-centered is-small'
+    render_inline(component) do |c|
+      c.item(href: '/home', name: 'Home', icon_name: 'home')
+    end
+
+    expect(subject).to have_css 'nav.breadcrumb-component.is-centered.is-small'
+  end
 end
