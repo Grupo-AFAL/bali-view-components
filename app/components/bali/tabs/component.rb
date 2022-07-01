6a314f560e0972e0a73563733cbcfbfaea609b05
Miguel Frias <migue_tj_frias@hotmail.com>
2022-07-01 15:59:38 -0700

    Full page reload on tab click

--- app/components/bali/tabs/component.rb
+++ app/components/bali/tabs/component.rb
@@ -3,9 +3,13 @@
 module Bali
   module Tabs
     class Component < ApplicationViewComponent
-      renders_many :tabs, Tab::Component
+      renders_many :tabs, ->(**options) {
+        Tab::Component.new(full_page_reload: @full_page_reload, **options)
+      } 
 
-      def initialize(**options)
+      def initialize(full_page_reload: false, **options)
+        @full_page_reload = full_page_reload
+
         @options = prepend_class_name(options, 'tabs-component')
         @options = prepend_controller(options, 'tabs')
 
--- app/components/bali/tabs/tab/component.rb
+++ app/components/bali/tabs/tab/component.rb
@@ -4,7 +4,7 @@ module Bali
   module Tabs
     module Tab
       class Component < ApplicationViewComponent
-        attr_reader :active, :icon, :title, :src, :reload, :options
+        attr_reader :active, :icon, :title, :src, :reload, :full_page_reload, :options
 
         # @param active [Boolean] Whether the tab is active
         # @param icon [String] The name of the icon to use
@@ -20,13 +20,52 @@ module Bali
           @title = title
           @src = src
           @reload = reload
+          @full_page_reload = options.delete(:full_page_reload)
 
           @options = options
           @options = prepend_class_name(@options, 'is-hidden') unless @active
         end
 
+        def trigger(index = 0)  
+          tag.li(data: trigger_li_data(index), class: trigger_li_classes) do
+
+            tag.a(**trigger_a_options) do
+              safe_join([
+                icon ? render(Bali::Icon::Component.new(icon)) : nil,
+                tag.span { title }
+              ])
+            end
+          end
+        end
+
         def call
           content
+        end
+
+        private
+
+        def trigger_li_data(index)
+          return {} if full_page_reload
+
+          { 
+            'tabs-target': 'tab',
+                           'tabs-index-param': index,
+                           'tabs-src-param': src,
+                           'tabs-reload-param': reload,
+                            action: 'click->tabs#open' 
+                          }
+        end
+
+        def trigger_li_classes
+          return class_names('is-active': active_path?(request.fullpath, src)) if full_page_reload
+
+          class_names('is-active': active)
+        end
+
+        def trigger_a_options
+          return { href: src } if full_page_reload
+
+          {}
         end
       end
     end
