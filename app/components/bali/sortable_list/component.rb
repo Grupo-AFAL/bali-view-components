759095c764957a88b0a19696aef03334f6dc11e6
Federico Gonzalez <fedegl@hey.com>
2022-07-01 15:27:40 -0700

    Reorder params and document them

--- app/components/bali/sortable_list/component.rb
+++ app/components/bali/sortable_list/component.rb
@@ -7,28 +7,38 @@ module Bali
 
       renders_many :items, Item::Component
 
+      # @param resource_name [String] name of the resource being updated. e.g. 'task'
+      # @param position_param_name [String] name of the position parameter sent to the server
+      # @param list_param_name [String] name of the list parameter sent to the server
+      # @param group_name [String] name to group multiple lists to be able to drag items across them
+      # @param list_id [String|Integer] Identifier for each list to move items between them
+      # @param response_kind [Symbol] :turbo_stream or :html
+      # @param handle [String] class_name for the handle
+      # @param disabled [Boolean] disabled moving of items
+      # @param animation [Integer] duration of the animation in miliseconds
+      #
       # rubocop:disable Metrics/ParameterLists
       def initialize(
-        animation: nil,
-        disabled: false,
-        group_name: nil,
-        handle: nil,
-        list_id: nil,
-        list_param_name: nil,
         resource_name: nil,
-        response_kind: nil,
-        position_param_name: nil,
+        position_param_name: 'position',
+        list_param_name: 'list_id',
+        group_name: nil,
+        list_id: nil,
+        response_kind: :html,
+        handle: nil,
+        disabled: false,
+        animation: 150,
         **options
       )
-        @animation = animation
-        @disabled = disabled
-        @group_name = group_name
-        @handle = handle
-        @list_id = list_id
-        @list_param_name = list_param_name
         @resource_name = resource_name
-        @response_kind = response_kind
         @position_param_name = position_param_name
+        @list_param_name = list_param_name
+        @group_name = group_name
+        @list_id = list_id
+        @response_kind = response_kind
+        @handle = handle
+        @disabled = disabled
+        @animation = animation
 
         @options = prepend_class_name(options, 'sortable-list-component')
         @options = prepend_controller(@options, 'sortable-list')
