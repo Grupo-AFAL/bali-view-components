db4da4566a81bef4e611d65790ea994e2636bc86
Ivan Aguilar <ivan96318@gmail.com>
2022-07-04 16:53:37 -0700

    add documnetation in previews

--- app/components/bali/avatar/preview.rb
+++ app/components/bali/avatar/preview.rb
@@ -2,16 +2,23 @@ module Bali
   module Avatar
     class Preview < ApplicationViewComponentPreview
       def default
+        # Default Avatar
+        # ----------------
+        # This will render the basic avatar component
         render_with_template(
           template: 'bali/avatar/previews/default'
         )
       end
 
-      def with_image
+      # Avatar with an image
+      # ----------------
+      # This will render the avatar component with an image loaded already
+      # @param image_url text
+      def with_image(image_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8qsnp3N8vzUr0SdcfX8ILtjAipw3bNA9DAg&usqp=CAU')
         render_with_template(
           template: 'bali/avatar/previews/with_image',
           locals: {
-            image_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8qsnp3N8vzUr0SdcfX8ILtjAipw3bNA9DAg&usqp=CAU'
+            image_url: image_url
           }
         )
       end
