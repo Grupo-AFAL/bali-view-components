00cb303ba43009863cd9d4b87b1ad6afdd876f24
Sergio Lopez <sergio@softnya.com>
2022-07-08 11:37:40 -0500

    Preview con opcion de delete

--- app/components/bali/tags/preview.rb
+++ app/components/bali/tags/preview.rb
@@ -13,7 +13,7 @@ module Bali
       # @param size [Symbol] select [small, normal, large]
       # @param light_color toggle
       # @param rounded toggle
-      def default(size: :normal, light_color: false, rounded: false)
+      def tags(size: :normal, light_color: false, rounded: false)
         render Tags::Component.new(
           sizes: size,
           all_light: light_color,
@@ -66,6 +66,73 @@ module Bali
           c.tag_item(
             text: 'Tag item with text',
             color: :danger
+          )
+        end
+      end
+
+      # Tag view as delete
+      # ---------------
+      # Delete tag view, juts specify `is_delete` to true on each item. Text will be ommited.
+      # 
+      # **Available sizes:** small, normal, large.
+      #
+      # **Available colors:** black, dark, light, white, primary, link, info, success, warning, danger.
+      # @param size [Symbol] select [small, normal, large]
+      # @param light_color toggle
+      # @param rounded toggle
+      def delete_tags(size: :normal, light_color: false, rounded: false)
+        render Tags::Component.new(
+          sizes: size,
+          all_light: light_color,
+          all_rounded: rounded
+        ) do |c|
+          c.tag_item(
+            is_delete: true
+          )
+
+          c.tag_item(
+            color: :black,
+            is_delete: true
+          )
+
+          c.tag_item(
+            color: :light,
+            is_delete: true
+          )
+
+          c.tag_item(
+            color: :white,
+            is_delete: true
+          )
+
+          c.tag_item(
+            color: :primary,
+            is_delete: true
+          )
+
+          c.tag_item(
+            color: :link,
+            is_delete: true
+          )
+
+          c.tag_item(
+            color: :info,
+            is_delete: true
+          )
+
+          c.tag_item(
+            color: :success,
+            is_delete: true
+          )
+
+          c.tag_item(
+            color: :warning,
+            is_delete: true
+          )
+          
+          c.tag_item(
+            color: :danger,
+            is_delete: true
           )
         end
       end
