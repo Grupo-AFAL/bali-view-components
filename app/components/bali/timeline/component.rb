16faa788ddb9c99190d842d4a3fa7b064e4d0c55
Federico Gonzalez <fedegl@hey.com>
2022-07-08 15:03:44 -0700

    E110 timeline component (#229)
    * Create timeline component
    
    * Use polymorphic slots to control position and add tests
    
    * Update README and bump version
    
    * Address feedback

---
+++ app/components/bali/timeline/component.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Bali
+  module Timeline
+    class Component < ApplicationViewComponent
+      include ViewComponent::PolymorphicSlots
+
+      attr_reader :options
+
+      renders_many :tags, types: {
+        header: Timeline::Header::Component,
+        item: Timeline::Item::Component
+      }
+
+      def initialize(position: :left, **options)
+        @position = position
+
+        @options = prepend_class_name(options, 'timeline-component')
+        @options = prepend_class_name(@options, 'is-centered') if position == :center
+        @options = prepend_class_name(@options, 'is-rtl') if position == :right
+      end
+    end
+  end
+end
---
+++ spec/bali/components/timeline_spec.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Bali::Timeline::Component, type: :component do
+  let(:options) { {} }
+  let(:component) { Bali::Timeline::Component.new(**options) }
+
+  it 'renders timeline component' do
+    render_inline(component)
+
+    expect(page).to have_css 'div.timeline-component'
+  end
+
+  it 'renders timeline center aligned' do
+    options[:position] = :center
+    render_inline(component)
+
+    expect(page).to have_css 'div.timeline-component.is-centered'
+  end
+
+  it 'renders timeline right aligned' do
+    options[:position] = :right
+    render_inline(component)
+
+    expect(page).to have_css 'div.timeline-component.is-rtl'
+  end
+
+  it 'renders timeline items with icons' do
+    render_inline(component) do |c|
+      c.with_tag_item(icon: 'alert')
+    end
+
+    expect(page).to have_css '.timeline-item', count: 1
+    expect(page).to have_css '.timeline-marker.is-icon'
+  end
+
+  it 'renders timeline items with heading' do
+    render_inline(component) do |c|
+      c.with_tag_item(heading: 'January 2022')
+    end
+
+    expect(page).to have_css '.timeline-item', count: 1
+    expect(page).to have_css '.timeline-content .heading', text: 'January 2022'
+  end
+
+  it 'renders timeline header' do
+    render_inline(component) do |c|
+      c.with_tag_header(text: 'Start')
+    end
+
+    expect(page).to have_css '.timeline-header', count: 1
+    expect(page).to have_css '.timeline-header .tag', text: 'Start'
+  end
+
+  it 'renders timeline header with tag_class' do
+    render_inline(component) do |c|
+      c.with_tag_header(text: 'Start', tag_class: 'is-primary')
+    end
+
+    expect(page).to have_css '.timeline-header', count: 1
+    expect(page).to have_css '.timeline-header .tag.is-primary', text: 'Start'
+  end
+end
