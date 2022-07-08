1050f2036084a3e98719d6e1b498c353e114c9d3
Sergio Lopez <sergio@softnya.com>
2022-07-08 11:30:10 -0500

    Completar preview de tags en lookbook

--- app/components/bali/tags/preview.rb
+++ app/components/bali/tags/preview.rb
@@ -3,8 +3,71 @@
 module Bali
   module Tags
     class Preview < ApplicationViewComponentPreview
-      def default
-        # Default preview goes here
+      # Tag view
+      # ---------------
+      # Basic tag view with text.
+      # 
+      # **Available sizes:** small, normal, large.
+      #
+      # **Available colors:** black, dark, light, white, primary, link, info, success, warning, danger.
+      # @param size [Symbol] select [small, normal, large]
+      # @param light_color toggle
+      # @param rounded toggle
+      def default(size: :normal, light_color: false, rounded: false)
+        render Tags::Component.new(
+          sizes: size,
+          all_light: light_color,
+          all_rounded: rounded
+        ) do |c|
+          c.tag_item(
+            text: 'Tag item with text'
+          )
+
+          c.tag_item(
+            text: 'Tag item with text',
+            color: :black
+          )
+
+          c.tag_item(
+            text: 'Tag item with text',
+            color: :light
+          )
+
+          c.tag_item(
+            text: 'Tag item with text',
+            color: :white
+          )
+
+          c.tag_item(
+            text: 'Tag item with text',
+            color: :primary
+          )
+
+          c.tag_item(
+            text: 'Tag item with text',
+            color: :link
+          )
+
+          c.tag_item(
+            text: 'Tag item with text',
+            color: :info
+          )
+
+          c.tag_item(
+            text: 'Tag item with text',
+            color: :success
+          )
+
+          c.tag_item(
+            text: 'Tag item with text',
+            color: :warning
+          )
+          
+          c.tag_item(
+            text: 'Tag item with text',
+            color: :danger
+          )
+        end
       end
     end
   end
