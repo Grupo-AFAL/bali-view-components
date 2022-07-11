b4018bab01b9a1cfb1534f35a5ece888378f027b
Sergio Lopez <sergio@softnya.com>
2022-07-11 10:40:47 -0500

    Preview de tag

--- app/components/bali/tag/preview.rb
+++ app/components/bali/tag/preview.rb
@@ -3,8 +3,84 @@
 module Bali
   module Tag
     class Preview < ApplicationViewComponentPreview
-      def default
-        # Default preview goes here
+      # Tag view
+      # ---------------
+      # Basic tag view with text.
+      # 
+      # **Available sizes:** small, normal, large.
+      #
+      # **Available colors:** black, dark, light, white, primary, link, info, success, warning, danger.
+      # @param size [Symbol] select [small, normal, large]
+      # @param light toggle
+      # @param rounded toggle
+      def tag(size: :normal, light: false, rounded: false)
+        render Tag::Component.new(
+          text: 'Tag item with text',
+          size: size,
+          light: light,
+          rounded: rounded
+        )
+      end
+
+      # Tag view
+      # ---------------
+      # Colored tag view with text.
+      # 
+      # **Available sizes:** small, normal, large.
+      #
+      # **Available colors:** black, dark, light, white, primary, link, info, success, warning, danger.
+      # @param size [Symbol] select [small, normal, large]
+      # @param light toggle
+      # @param rounded toggle
+      def tag_color(size: :normal, light: false, rounded: false)
+        render Tag::Component.new(
+          text: 'Tag item with text',
+          color: :black,
+          size: size,
+          light: light,
+          rounded: rounded
+        )
+      end
+
+      # Tag view
+      # ---------------
+      # Delete tag view with text.
+      # 
+      # **Available sizes:** small, normal, large.
+      #
+      # **Available colors:** black, dark, light, white, primary, link, info, success, warning, danger.
+      # @param size [Symbol] select [small, normal, large]
+      # @param light toggle
+      # @param rounded toggle
+      def tag_delete_with_text(size: :normal, light: false, rounded: false)
+        render Tag::Component.new(
+          text: 'Tag item with text',
+          delete: true,
+          color: :black,
+          size: size,
+          light: light,
+          rounded: rounded
+        )
+      end
+
+      # Tag view
+      # ---------------
+      # Delete tag view without text.
+      # 
+      # **Available sizes:** small, normal, large.
+      #
+      # **Available colors:** black, dark, light, white, primary, link, info, success, warning, danger.
+      # @param size [Symbol] select [small, normal, large]
+      # @param light toggle
+      # @param rounded toggle
+      def tag_delete_without_text(size: :normal, light: false, rounded: false)
+        render Tag::Component.new(
+          delete: true,
+          color: :black,
+          size: size,
+          light: light,
+          rounded: rounded
+        )
       end
     end
   end
