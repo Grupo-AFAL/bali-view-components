bd6ed231fc579e973e694a7eb644f99a31df1ffe
alexisKinto <mgutierrez@afal.mx>
2022-07-11 15:41:32 -0700

    updating default title and subtitle sizes, and adding a type params

--- app/components/bali/page_header/component.rb
+++ app/components/bali/page_header/component.rb
@@ -5,20 +5,39 @@ module Bali
     class Component < ApplicationViewComponent
       attr_reader :options
 
-      renders_one :title, ->(text = nil, **options, &block) do
-        options[:class] ||= 'title is-1 mb-0'
-        text.present? ? tag.h1(text, **options) : tag.div(&block)
+      renders_one :title, ->(text = nil, type: :h3, **options, &block) do
+        options = prepend_class_name(options, class_names('title', heading_size_class(type)))
+
+        heading_tag(text, type, **options, &block)
       end
 
-      renders_one :subtitle, ->(text = nil, **options, &block) do
-        options[:class] ||= 'subtitle mt-0 is-6'
-        text.present? ? tag.p(text, **options) : tag.div(&block)
+      renders_one :subtitle, ->(text = nil, type: :h5, **options, &block) do
+        options = prepend_class_name(options, class_names('subtitle', heading_size_class(type)))
+
+        heading_tag(text, type, **options, &block)
       end
 
       def initialize(title: nil, subtitle: nil, **options)
         @title = title
         @subtitle = subtitle
         @options = prepend_class_name(options, 'page-header-component')
+      end
+
+      def heading_size_class(type)
+        {
+          'is-1': type == :h1,
+          'is-2': type == :h2,
+          'is-3': type == :h3,
+          'is-4': type == :h4,
+          'is-5': type == :h5,
+          'is-6': type == :h6
+        }
+      end
+
+      def heading_tag(text, type, **options, &)
+        size = type.to_s.gsub(/^h/, '')
+
+        text.present? ? tag.send("h#{size}", text, **options) : tag.div(**options, &)
       end
     end
   end
--- app/components/bali/page_header/preview.rb
+++ app/components/bali/page_header/preview.rb
@@ -25,10 +25,17 @@ module Bali
 
       # @param title text
       # @param subtitle text
-      def with_title_and_subtitle_as_slots(title: 'Title', subtitle: 'Subtitle')
+      # @param title_type select ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
+      # @param subtitle_type select ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
+      # @param title_class select ['has-text-info', 'has-text-success', 'has-text-warning']
+      # @param subtitle_class select ['has-text-primary', 'has-text-danger', 'has-text-link']
+      def with_title_and_subtitle_as_slots(
+        title: 'Title', title_type: :h3, title_class: 'has-text-info',
+        subtitle: 'Subtitle', subtitle_type: :h5, subtitle_class: 'has-text-primary'
+      )
         render PageHeader::Component.new do |c|
-          c.title(title, class: 'title is-3')
-          c.subtitle(subtitle, class: 'subtitle is-6')
+          c.title(title, type: title_type.to_sym, class: title_class)
+          c.subtitle(subtitle, type: subtitle_type.to_sym, class: subtitle_class)
 
           tag.a 'Right action', class: 'button is-secondary', href: '#'
         end
@@ -38,8 +45,8 @@ module Bali
       # @param subtitle text
       def with_title_and_subtitle_as_block(title: 'Title', subtitle: 'Subtitle')
         render PageHeader::Component.new do |c|
-          c.title { tag.h1(title, class: 'title is-3') }
-          c.subtitle { tag.p(subtitle, class: 'subtitle is-6') }
+          c.title { tag.h3(title, class: 'title is-3') }
+          c.subtitle { tag.h5(subtitle, class: 'subtitle is-5') }
 
           tag.a 'Right action', class: 'button is-secondary', href: '#'
         end
