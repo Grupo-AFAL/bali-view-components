7897f01f5dfbea93691e81b2d935644d7d2cc492
Sergio Lopez <sergio@softnya.com>
2022-07-11 15:38:01 -0500

    Correcciones en pruebas de tag y tags

--- spec/bali/components/tag_spec.rb
+++ spec/bali/components/tag_spec.rb
@@ -9,54 +9,44 @@ RSpec.describe Bali::Tag::Component, type: :component do
   subject { page }
 
   describe 'rendering tag with' do
-    context '' do
-      before { @options.merge!(text: 'Tag item with text') }
+    before { @options.merge!(text: 'Tag item with text') }
 
-      it 'single item text' do
-        render_inline(component)
+    it 'single item text' do
+      render_inline(component)
 
-        expect(page).to have_css 'div.tag-component.tag', text: 'Tag item with text'
-      end
+      expect(page).to have_css 'div.tag-component.tag', text: 'Tag item with text'
     end
 
-    context '' do
-      before { @options.merge!(text: 'Tag item with text', color: :black) }
+    before { @options.merge!(text: 'Tag item with text', color: :black) }
 
-      it 'single item color' do
-        render_inline(component)
+    it 'single item color' do
+      render_inline(component)
 
-        expect(page).to have_css 'div.is-black.tag-component.tag', text: 'Tag item with text'
-      end
+      expect(page).to have_css 'div.is-black.tag-component.tag', text: 'Tag item with text'
     end
 
-    context '' do
-      before { @options.merge!(text: 'Tag item with text', size: :small) }
+    before { @options.merge!(text: 'Tag item with text', size: :small) }
 
-      it 'single item size' do
-        render_inline(component)
+    it 'single item size' do
+      render_inline(component)
 
-        expect(page).to have_css 'div.tag-component.tag.is-small', text: 'Tag item with text'
-      end
+      expect(page).to have_css 'div.tag-component.tag.is-small', text: 'Tag item with text'
     end
 
-    context '' do
-      before { @options.merge!(text: 'Tag item with text', light: true) }
+    before { @options.merge!(text: 'Tag item with text', light: true) }
 
-      it 'single item light' do
-        render_inline(component)
+    it 'single item light' do
+      render_inline(component)
 
-        expect(page).to have_css 'div.tag-component.tag.is-light', text: 'Tag item with text'
-      end
+      expect(page).to have_css 'div.tag-component.tag.is-light', text: 'Tag item with text'
     end
 
-    context '' do
-      before { @options.merge!(text: 'Tag item with text', rounded: true) }
+    before { @options.merge!(text: 'Tag item with text', rounded: true) }
 
-      it 'single item rounded' do
-        render_inline(component)
+    it 'single item rounded' do
+      render_inline(component)
 
-        expect(page).to have_css 'div.tag-component.tag.is-rounded', text: 'Tag item with text'
-      end
+      expect(page).to have_css 'div.tag-component.tag.is-rounded', text: 'Tag item with text'
     end
   end
 end
--- spec/bali/components/tags_spec.rb
+++ spec/bali/components/tags_spec.rb
@@ -5,47 +5,45 @@ require 'rails_helper'
 RSpec.describe Bali::Tags::Component, type: :component do
   let(:component) { Bali::Tags::Component.new }
 
-  describe 'rendering' do
-    context 'tags with' do
-      it 'single item text' do
-        render_inline(component) do |c|
-          c.item(text: 'Tag item with text')
-        end
-
-        expect(page).to have_css 'div.tag-component.tag', text: 'Tag item with text'
+  describe 'rendering tags with' do
+    it 'single item text' do
+      render_inline(component) do |c|
+        c.item(text: 'Tag item with text')
       end
 
-      it 'single item color' do
-        render_inline(component) do |c|
-          c.item(text: 'Tag item with text', color: :black)
-        end
+      expect(page).to have_css 'div.tag-component.tag', text: 'Tag item with text'
+    end
 
-        expect(page).to have_css 'div.is-black.tag-component.tag', text: 'Tag item with text'
+    it 'single item color' do
+      render_inline(component) do |c|
+        c.item(text: 'Tag item with text', color: :black)
       end
 
-      it 'single item size' do
-        render_inline(component) do |c|
-          c.item(text: 'Tag item with text', size: :small)
-        end
+      expect(page).to have_css 'div.is-black.tag-component.tag', text: 'Tag item with text'
+    end
 
-        expect(page).to have_css 'div.tag-component.tag.is-small', text: 'Tag item with text'
+    it 'single item size' do
+      render_inline(component) do |c|
+        c.item(text: 'Tag item with text', size: :small)
       end
 
-      it 'single item light' do
-        render_inline(component) do |c|
-          c.item(text: 'Tag item with text', light: true)
-        end
+      expect(page).to have_css 'div.tag-component.tag.is-small', text: 'Tag item with text'
+    end
 
-        expect(page).to have_css 'div.tag-component.tag.is-light', text: 'Tag item with text'
+    it 'single item light' do
+      render_inline(component) do |c|
+        c.item(text: 'Tag item with text', light: true)
       end
 
-      it 'single item rounded' do
-        render_inline(component) do |c|
-          c.item(text: 'Tag item with text', rounded: true)
-        end
+      expect(page).to have_css 'div.tag-component.tag.is-light', text: 'Tag item with text'
+    end
 
-        expect(page).to have_css 'div.tag-component.tag.is-rounded', text: 'Tag item with text'
+    it 'single item rounded' do
+      render_inline(component) do |c|
+        c.item(text: 'Tag item with text', rounded: true)
       end
+
+      expect(page).to have_css 'div.tag-component.tag.is-rounded', text: 'Tag item with text'
     end
   end
 end
