197db89fb463dfcf0a88376a4cc304e5057c0b45
Federico Gonzalez <fedegl@hey.com>
2022-07-18 11:07:24 -0700

    Add additional match option to SideMenu Item

--- lib/bali/path_helper.rb
+++ lib/bali/path_helper.rb
@@ -2,6 +2,15 @@
 
 module Bali
   module PathHelper
+    # Returns true when the path matches the current path depending on the match type
+    # Available match types are
+    # - exact: Matches the whole path
+    # - partial: Matches if the path is included in the current path
+    # - starts_with: Matches if the current path starts with the path
+    #
+    # @param path [String] represents the path for the link
+    # @param current_path [String] currently active URL
+    # @param match [Symbol] one of 3 match types [:exact, :partial, :starts_with]
     def active_path?(path, current_path, match: :exact)
       return false if current_path.nil?
 
@@ -9,6 +18,8 @@ module Bali
       current_request_path = current_path.gsub(/\.html$/, '')
 
       case match
+      when :starts_with
+        current_request_path.starts_with?(path)
       when :partial
         current_request_path.include?(path)
       else
