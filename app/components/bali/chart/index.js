6ebfc9b206b7df356baf471abea394463cc1068b
Miguel Frias <31300198+MiguelFrias97@users.noreply.github.com>
2022-07-28 12:17:51 -0700

    Remove chartjs dependency

--- app/components/bali/chart/index.js
+++ app/components/bali/chart/index.js
@@ -1,4 +1,48 @@
-import Chart from 'stimulus-chartjs'
+import { Controller } from '@hotwired/stimulus'
+import Chart from 'chart.js/auto'
 
-export class ChartController extends Chart {
+export class ChartController extends Controller {
+  static targets = ['canvas']
+  static values = {
+    type: {
+      type: String,
+      default: 'line'
+    },
+    data: Object,
+    options: Object
+  }
+
+  connect () {
+    const element = this.hasCanvasTarget ? this.canvasTarget : this.element
+
+    this.chart = new Chart(element.getContext('2d'), {
+      type: this.typeValue,
+      data: this.chartData,
+      options: this.chartOptions
+    })
+  }
+
+  disconnect () {
+    this.chart.destroy()
+    this.chart = undefined
+  }
+
+  get chartData () {
+    if (!this.hasDataValue) {
+      console.warn('[stimulus-chartjs] You need to pass data as JSON to see the chart.')
+    }
+
+    return this.dataValue
+  }
+
+  get chartOptions () {
+    return {
+      ...this.defaultOptions,
+      ...this.optionsValue
+    }
+  }
+
+  get defaultOptions () {
+    return {}
+  }
 }
