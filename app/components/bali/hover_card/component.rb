018ef496a1242708fe1d0be3f72b53d35f7f7b71
Federico Gonzalez <fedegl@hey.com>
2022-08-03 12:27:42 -0700

    Enable activating hovercard on click instead of hover (#246)
    * Enable activating hovercard on click instead of hover
    
    * add test to hovercard component to check the open-on-click property and update the version
    
    * Modify offset
    
    * remove package-lock.json
    
    Co-authored-by: Ivan Aguilar <ivan96318@gmail.com>

--- app/components/bali/hover_card/component.rb
+++ app/components/bali/hover_card/component.rb
@@ -3,7 +3,7 @@
 module Bali
   module HoverCard
     class Component < ApplicationViewComponent
-      attr_reader :placement, :options, :data, :hover_url
+      attr_reader :options
 
       renders_one :trigger, ->(**options, &block) do
         options = prepend_class_name(options, 'hovercard-component--trigger')
@@ -13,14 +13,17 @@ module Bali
 
       renders_one :template
 
-      def initialize(hover_url: nil, placement: 'auto', **options)
+      def initialize(hover_url: nil, placement: 'auto', open_on_click: false, **options)
         @placement = placement
         @data = options.delete(:data) || {}
         @hover_url = options.dig(:data, :'hovercard-url-value') || hover_url
+        @open_on_click = open_on_click
+
         @options = prepend_class_name(options, 'hover-card-component')
         @options = prepend_controller(@options, 'hovercard')
         @options = prepend_data_attribute(@options, 'hovercard-placement-value', placement)
         @options = prepend_data_attribute(@options, 'hovercard-url-value', hover_url)
+        @options = prepend_data_attribute(@options, 'hovercard-open-on-click-value', open_on_click)
       end
     end
   end
