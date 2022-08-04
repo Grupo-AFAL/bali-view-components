77b05f074f5890a764463eead11a9314aa6d7e89
Ivan Aguilar <ivan96318@gmail.com>
2022-08-04 09:30:01 -0700

    fix typo and add documentation about the new functionality

--- app/javascript/bali/controllers/radio-toggle-controller.js
+++ app/javascript/bali/controllers/radio-toggle-controller.js
@@ -15,6 +15,23 @@ import { Controller } from '@hotwired/stimulus'
  *     <h1 class="title is-1">Two</h1>
  *   </div>
  * </div>
+ *
+ *
+ * Shows the same result with multiple radio buttons value
+ *
+ * <div data-controller="radio-toggle" data-toggle-radio-current-value="one">
+ *   <input type="radio" data-action="radio-toggle#change" value="one">
+ *   <input type="radio" data-action="radio-toggle#change" value="two">
+ *
+ *   <div data-radio-toggle-target="element" data-radio-toggle-value="one">
+ *     <h1 class="title is-1">One</h1>
+ *   </div>
+ *
+ *   <div data-radio-toggle-target="element" data-radio-toggle-value="one,two">
+ *     <h1 class="title is-1">Two</h1>
+ *   </div>
+ * </div>
+ *
  */
 
 // TODO: Add tests (Issue: #143)
@@ -32,9 +49,9 @@ export class RadioToggleController extends Controller {
 
   toggleTargets (value) {
     this.elementTargets.forEach(element => {
-      const valuesPorperties = element.dataset.radioToggleValue.split(',')
+      const valuesProperties = element.dataset.radioToggleValue.split(',')
 
-      if (valuesPorperties.includes(value)) {
+      if (valuesProperties.includes(value)) {
         element.classList.remove('is-hidden')
       } else {
         element.classList.add('is-hidden')
