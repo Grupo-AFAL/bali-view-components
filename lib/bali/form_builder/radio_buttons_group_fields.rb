f0c81fc3a2fe362cd622c50b465af94ded2a6f69
Sergio Lopez <sergio@softnya.com>
2022-08-04 12:49:44 -0500

    Reestructuracion de botones para uso de controlador toggler

--- lib/bali/form_builder/radio_buttons_group_fields.rb
+++ lib/bali/form_builder/radio_buttons_group_fields.rb
@@ -5,42 +5,41 @@ module Bali
     module RadioButtonsGroupFields
       def radio_buttons_group(method, values, options = {}, html_options = {})
         options.with_defaults!(
-          data: {},
-          class: ''
+          toggler: {
+            type: :radio,
+            data: { action: 'radio-toggle#change' }
+          }
         )
         selected = options.delete(:selected) || nil
-        rounded = options.delete(:rounded) || false
-        classes = class_names('tabs is-toggle tabs-fb', 'is-toggle-rounded': rounded)
+        radio_name = options.delete(:radio_name) || ''
 
         @template.safe_join([
           @template.content_tag(:div, data: {
-            controller: 'toggle-radio',
-            'toggle-radio-current-value': selected
-          }) {[
-            @template.content_tag(:div, class: classes) {
-              @template.content_tag(:ul) do
-                @template.safe_join(buttons(values, nil, options))
-              end
-            },
-
-            @template.content_tag(:div, class: 'tab-content tabs-fb') {
-              options[:div_class] = classes
-              @template.safe_join(contents(values, selected, options))
-            }
-          ]}
+            controller: 'radio-toggle',
+            'toggle-radio-current-value': values.first.first
+          }) {
+            @template.safe_join(buttons(values, nil, radio_name, options))
+          }
         ])
       end
 
       private
 
-      def buttons(values, selected, options)
+      def buttons(values, selected, radio_name, options)
         values.map.with_index do |value, index|
-          active = class_names('is-active': selected == value.last || (index == 0 && selected.blank?)) || ''
-          @template.content_tag(:li, data: { tab: index, value: value.last }, class: active) do
-            @template.content_tag(:a, '', options) do
-              @template.content_tag(:span, value.first)
-            end
-          end
+          toggler_class = class_names('button mr-2', 'is-active': selected == value.last || (index == 0 && selected.blank?)) || ''
+          @template.safe_join([
+            @template.content_tag(:input, '', options[:toggler].with_defaults(
+              class: 'is-h idden',
+              name: 'radio-toggle-button-' + radio_name,
+            )),
+            @template.content_tag(:label, value.first, { class: toggler_class, for: 'radio-toggle-button-' + value.first })
+          ])
+          #@template.content_tag(:li, data: { tab: index, value: value.last }, class: active) do
+          #  @template.content_tag(:a, '', options) do
+          #    @template.content_tag(:span, value.first)
+          #  end
+          #end
         end
       end
 
