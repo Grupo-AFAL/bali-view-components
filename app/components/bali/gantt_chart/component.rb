d730b1c92e7dbcad321eed20be033a3ea55d003b
Federico Gonzalez <fedegl@hey.com>
2022-08-08 15:49:21 -0700

    Fix rubocop and rspec

--- app/components/bali/gantt_chart/component.rb
+++ app/components/bali/gantt_chart/component.rb
@@ -15,6 +15,9 @@ module Bali
         @options = prepend_action(options, 'sortable-list:onEnd->gantt-chart#onItemReordered')
         @options = prepend_action(options, 'interact:onResizeEnd->gantt-chart#onItemResized')
         @options = prepend_action(options, 'interact:onDragEnd->gantt-chart#onItemDragged')
+
+        @default_start_date = Date.current.beginning_of_month - 1.month
+        @default_end_date = Date.current.end_of_month + 2.months
       end
 
       def before_render
@@ -29,7 +32,7 @@ module Bali
       end
 
       def end_date
-        [max_date.end_of_month + 1.month, (start_date + 5.months).end_of_month].max
+        max_date.end_of_month + 1.month
       end
 
       def duration
@@ -37,15 +40,23 @@ module Bali
       end
 
       def min_date
-        @min_date ||= tasks.min { |a, b| a.start_date <=> b.start_date }.start_date
+        @min_date ||= earliest_task&.start_date || @default_start_date
       end
 
       def max_date
-        @max_date ||= tasks.max { |a, b| a.end_date <=> b.end_date }.end_date
+        @max_date ||= latest_task&.end_date || @default_end_date
       end
 
       def days_by_month
         (start_date..end_date).group_by(&:month)
+      end
+
+      def earliest_task
+        tasks.min { |a, b| a.start_date <=> b.start_date }
+      end
+
+      def latest_task
+        tasks.max { |a, b| a.end_date <=> b.end_date }
       end
     end
   end
--- spec/bali/components/gantt_chart_spec.rb
+++ spec/bali/components/gantt_chart_spec.rb
@@ -3,12 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe Bali::GanttChart::Component, type: :component do
-  let(:options) { {  } }
+  let(:options) { {} }
   let(:component) { Bali::GanttChart::Component.new(**options) }
 
-  it 'renders ganttchart component' do
+  it 'renders gantt chart component' do
     render_inline(component)
 
-    expect(page).to have_css 'div.gantt_chart-component'
+    expect(page).to have_css 'div.gantt-chart-component'
   end
 end
