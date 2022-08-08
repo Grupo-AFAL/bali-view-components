d92ea72b8e42936e169545b69af8027c212e768f
Federico Gonzalez <fedegl@hey.com>
2022-08-08 15:15:21 -0700

    Implement resizing without interactjs

--- app/javascript/bali/controllers/resizer-controller.js
+++ app/javascript/bali/controllers/resizer-controller.js
@@ -14,78 +14,120 @@ export class ResizerController extends Controller {
   }
 
   setup () {
-    console.log('setup', this.interactable)
+    // console.log('setup', this.interactable)
 
     this.width = this.element.clientWidth
     this.positionX = 0
-    this.interactable = interact(this.element)
-      .resizable({
-        edges: {
-          top: false,
-          left: true,
-          bottom: false,
-          right: true
-        },
-        onmove: this.onResize,
-        onend: this.onResizeEnd
-      })
-      .draggable({
-        onmove: this.onDrag,
-        onend: this.onDragEnd
-      })
+    // this.interactable = interact(this.element)
+    //   .resizable({
+    //     edges: {
+    //       top: false,
+    //       left: true,
+    //       bottom: false,
+    //       right: true
+    //     },
+    //     onmove: this.onResize,
+    //     onend: this.onResizeEnd
+    //   })
+    //   .draggable({
+    //     onmove: this.onDrag,
+    //     onend: this.onDragEnd
+    //   })
   }
 
   reset () {
-    console.log('reset', this.interactable)
+    // console.log('reset', this.interactable)
 
-    this.interactable.unset()
-    this.interactable = undefined
+    // this.interactable.unset()
+    // this.interactable = undefined
 
     this.setup()
   }
 
-  onResize = event => {
-    let { x } = event.target.dataset
+  onResizeStart = event => {
+    console.log('onResizeStart', event.clientX)
+    event.preventDefault()
 
-    x = (parseFloat(x) || 0) + event.deltaRect.left
+    this.positionX = event.clientX
+    this.handle = event.params.handle
 
-    console.log({ x, width: this.width, position: this.positionValue })
+    document.onmousemove = this.onResizing
+    document.onmouseup = this.onResizeEnd
+  }
 
-    Object.assign(event.target.style, {
-      width: `${event.rect.width}px`,
-      transform: `translateX(${x}px)`
-    })
+  onResizing = event => {
+    event.preventDefault()
 
-    Object.assign(event.target.dataset, { x })
+    // let { x } = event.target.dataset
+
+    // x = (parseFloat(x) || 0) + event.deltaRect.left
+
+    // console.log({ x, width: this.width, position: this.positionValue })
+    let diffX, left, width
+    if (this.handle === 'left') {
+      diffX = this.positionX - event.clientX
+      left = this.positionValue - diffX
+      width = this.width + diffX
+    } else {
+      diffX = event.clientX - this.positionX
+      left = this.positionValue
+      width = this.width + diffX
+    }
+
+    this.element.style.left = `${left}px`
+    this.element.style.width = `${width}px`
+
+    // Object.assign(event.target.style, {
+    //   width: `${event.rect.width}px`,
+    //   transform: `translateX(${x}px)`
+    // })
+
+    // Object.assign(event.target.dataset, { x })
   }
 
   onResizeEnd = event => {
-    let { x } = event.target.dataset
+    // let { x } = event.target.dataset
 
-    console.log({ x })
+    // console.log({ x })
 
-    const width = this.snap(event.rect.width)
-    x = this.snap(x)
+    // const width = this.snap(event.rect.width)
+    // x = this.snap(x)
 
-    const left = this.positionValue + x
+    // const left = this.positionValue + x
 
-    Object.assign(event.target.style, {
-      width: `${width}px`,
-      left: `${left}px`,
-      transform: 'translateX(0px)'
-    })
+    // Object.assign(event.target.style, {
+    //   width: `${width}px`,
+    //   left: `${left}px`,
+    //   transform: 'translateX(0px)'
+    // })
 
-    const startDelta = x
-    const endDelta = width - this.width + x
+    // const startDelta = x
+    // const endDelta = width - this.width + x
 
-    this.positionValue = left
-    this.width = width
+    // this.positionValue = left
+    // this.width = width
 
-    console.log('onend', { startDelta, endDelta })
+    // console.log('onend', { startDelta, endDelta })
 
-    this.dispatch('onResizeEnd', { startDelta, endDelta })
+    // this.dispatch('onResizeEnd', { startDelta, endDelta })
 
-    Object.assign(event.target.dataset, { x: 0 })
+    // Object.assign(event.target.dataset, { x: 0 })
+
+    if (this.handle === 'left') {
+      const diffX = this.positionX - event.clientX
+      this.positionValue = this.snap(this.positionValue - diffX)
+      this.width = this.snap(this.width + diffX)
+    } else {
+      const diffX = event.clientX - this.positionX
+      this.width = this.snap(this.width + diffX)
+    }
+
+    this.element.style.left = `${this.positionValue}px`
+    this.element.style.width = `${this.width}px`
+    this.positionX = 0
+
+    document.onmouseup = null
+    document.onmousemove = null
   }
 
   onDrag = event => {
