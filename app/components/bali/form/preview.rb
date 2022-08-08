70de6285348f141bd9c4f610092421b08a58394d
Miguel Frias <31300198+MiguelFrias97@users.noreply.github.com>
2022-08-08 14:09:39 -0700

    Solve comments

--- app/components/bali/form/preview.rb
+++ app/components/bali/form/preview.rb
@@ -3,22 +3,21 @@
 module Bali
   module Form
     class Preview < ApplicationViewComponentPreview
-      FORM = Bali::Utils::DummyFilterForm.new
       VALUES = { 
-                'Cities': [
-                            ['Tijuana', 0],
-                            ['New York', 1],
-                            ['Brasilia', 2]
+                'Monday': [
+                            ['Sit-ups', 0],
+                            ['Push-ups', 1],
+                            ['Back Squats', 2]
                           ],
-                'States': [
-                            ['Baja California', 3],
-                            ['New York', 4],
-                            ['Central-West', 5],
+                'Tuesday': [
+                            ['Sit-ups', 3],
+                            ['Pull-ups', 4],
+                            ['Front-Squats', 5]
                           ],
-                'Countries': [
-                               ['Mexico', 6],
-                               ['USA', 7],
-                               ['Brazil', 8],
+                'Wednesday': [
+                               ['Hang cleans', 6],
+                               ['Runnning', 7],
+                               ['Clean & Jerks', 8]
                              ]
               }
 
@@ -26,7 +25,7 @@ module Bali
       def radio_buttons_field_group(keep_selection: false)
         render_with_template(
           template: 'bali/form/previews/radio_buttons_field_group',
-          locals: { model: FORM, values: VALUES, keep_selection: keep_selection }
+          locals: { model: Workout.new, values: VALUES, keep_selection: keep_selection }
         )
       end
 
@@ -34,7 +33,7 @@ module Bali
       def radio_buttons_field(keep_selection: false)
         render_with_template(
           template: 'bali/form/previews/radio_buttons_field',
-          locals: { model: FORM, values: VALUES, keep_selection: keep_selection  }
+          locals: { model: Workout.new, values: VALUES, keep_selection: keep_selection  }
         )
       end
     end
