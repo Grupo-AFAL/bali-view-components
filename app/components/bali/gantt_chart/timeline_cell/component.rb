2e4f35c51db5369af5d577b488f7dfb173ddba2a
Federico Gonzalez <fedegl@hey.com>
2022-08-15 12:36:44 -0700

    Pass in task options to the timeline cell component

--- app/components/bali/gantt_chart/timeline_cell/component.rb
+++ app/components/bali/gantt_chart/timeline_cell/component.rb
@@ -4,13 +4,26 @@ module Bali
   module GanttChart
     module TimelineCell
       class Component < ApplicationViewComponent
-        attr_reader :task, :readonly, :zoom, :task_name_padding
+        attr_reader :task, :readonly, :zoom, :task_name_padding, :options
 
         def initialize(task:, readonly:, zoom:)
           @task = task
           @readonly = readonly
           @zoom = zoom
           @task_name_padding = 8
+
+          @options = prepend_class_name(task.options, class_names(
+            'gantt-chart-cell',
+            milestone: task.milestone?,
+            'has-children': task.children.any?
+          ))
+          @options[:style] = "left: #{task.position_left}px; width: #{task.width}px"
+
+          @options = prepend_controller(@options, 'interact')
+          @options = prepend_data_attribute(@options, 'task-id', task.id)
+          @options = prepend_data_attribute(@options, 'parent-id', task.id)
+          @options = prepend_values(@options, 'interact', { position: task.position_left, width: task.width, params: task.params })
+          @options = prepend_data_attribute(@options, 'gantt-chart-target', 'timelineCell')
         end
 
         def resize_handle?
