f42bd2ee612279ac9199119c29b513a05ebd41cc
Miguel Frias <31300198+MiguelFrias97@users.noreply.github.com>
2022-08-24 10:56:19 -0700

    Fix rubocop

--- app/components/bali/table/component.rb
+++ app/components/bali/table/component.rb
@@ -36,7 +36,7 @@ module Bali
 
       def empty_table_row_id
         empty_row_id = 'empty-table-row'
-        id ? "#{id}-#{empty_row_id}": empty_row_id
+        id ? "#{id}-#{empty_row_id}" : empty_row_id
       end
     end
   end
