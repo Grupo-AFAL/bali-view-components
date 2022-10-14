854494b2437fee7fc19324f0c68ef32c3daf675b
Federico Gonzalez <fedegl@hey.com>
2022-10-14 13:15:12 -0700

    Add URL field group form helper (#293)

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_09_203912) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_14_193821) do
   create_table "characters", force: :cascade do |t|
     t.string "name"
     t.integer "movie_id", null: false
@@ -34,6 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_09_203912) do
     t.integer "time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "url"
   end
 
   create_table "movies", force: :cascade do |t|
