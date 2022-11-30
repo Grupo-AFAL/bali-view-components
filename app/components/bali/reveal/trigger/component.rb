01b7f09d70fb74a61508e7ed1e2fc2a2b05d3e47
Miguel Frias <31300198+MiguelFrias97@users.noreply.github.com>
2022-11-30 15:43:50 -0800

    Solve comments

--- app/components/bali/reveal/trigger/component.rb
+++ app/components/bali/reveal/trigger/component.rb
@@ -4,12 +4,11 @@ module Bali
   module Reveal
     module Trigger
       class Component < ApplicationViewComponent
-        attr_reader :title, :title_icon_url, :options
+        attr_reader :options
 
-        def initialize(title:, show_border: true, title_icon_url: nil, **options)
-          @title = title
-          @title_icon_url = title_icon_url
-          @title_class = options.delete(:title_class)
+        renders_one :title
+
+        def initialize(show_border: true, **options)
           @icon_class = options.delete(:icon_class)
 
           @options = prepend_class_name(options, 'reveal-trigger')
