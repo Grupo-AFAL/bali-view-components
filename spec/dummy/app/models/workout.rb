d22f02d23cf85dda51be884ae6260fd66f80634d
Federico Gonzalez <fedegl@hey.com>
2022-12-02 12:19:24 -0800

    Fix radio_field errors UI

--- spec/dummy/app/models/workout.rb
+++ spec/dummy/app/models/workout.rb
@@ -4,4 +4,6 @@ class Workout < ApplicationRecord
   attribute :name
   attribute :workout_start_at, TimeValue.new
   attribute :workout_end_at, TimeValue.new
+
+  validates :name, presence: true
 end
