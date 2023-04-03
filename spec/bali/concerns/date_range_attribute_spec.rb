6d5663ad329c1470cf99d6af53aa001c3f014799
Miguel Frias <31300198+MiguelFrias97@users.noreply.github.com>
2023-04-03 13:03:06 -0700

    Fix rubocop

--- spec/bali/concerns/date_range_attribute_spec.rb
+++ spec/bali/concerns/date_range_attribute_spec.rb
@@ -21,39 +21,39 @@ end
 RSpec.describe Bali::Concerns::DateRangeAttribute do
   context 'without start_attribute and end_attribute' do
     let(:model) { ActiveModelWithDateRangeAttribute.new }
-  
+
     describe '#date_range' do
       before { model.date_range = Time.zone.now.all_day }
-  
+
       it { expect(model.date_range).to eq(Time.zone.now.all_day) }
     end
-  
+
     describe '#date_range=' do
       context 'with range' do
         before { model.date_range = Time.zone.now.all_day }
-  
+
         it { expect(model.date_range).to eq(Time.zone.now.all_day) }
       end
-  
+
       context 'with a valid date range format' do
         let(:range) { Time.zone.local(2023, 3, 1)..Time.zone.local(2023, 3, 10).end_of_day }
-  
+
         before { model.date_range = '2023-03-01 to 2023-03-10' }
-  
+
         it { expect(model.date_range).to eq(range) }
       end
-  
+
       context 'with a day as a range' do
         let(:range) { Time.zone.local(2023, 3, 1)..Time.zone.local(2023, 3, 1).end_of_day }
-  
+
         before { model.date_range = '2023-03-01' }
-  
+
         it { expect(model.date_range).to eq(range) }
       end
-  
+
       context 'with date range missing' do
         before { model.date_range = '' }
-  
+
         it { expect(model.date_range).to be_nil }
       end
     end
@@ -61,45 +61,45 @@ RSpec.describe Bali::Concerns::DateRangeAttribute do
 
   context 'with start_attribute and end_attribute' do
     let(:model) { Workout.new }
-  
+
     describe '#date_range' do
       before { model.date_range = Time.zone.now.all_day }
-  
+
       it { expect(model.date_range).to eq(Time.zone.now.all_day) }
       it { expect(model.starts_at).to eq(Time.zone.now.beginning_of_day) }
       it { expect(model.ends_at).to eq(Time.zone.now.end_of_day) }
     end
-  
+
     describe '#date_range=' do
       context 'with range' do
         before { model.date_range = Time.zone.now.all_day }
-  
+
         it { expect(model.date_range).to eq(Time.zone.now.all_day) }
       end
-  
+
       context 'with a valid date range format' do
         let(:range) { Time.zone.local(2023, 3, 1)..Time.zone.local(2023, 3, 10).end_of_day }
-  
+
         before { model.date_range = '2023-03-01 to 2023-03-10' }
-  
+
         it { expect(model.date_range).to eq(range) }
         it { expect(model.starts_at).to eq(Time.zone.local(2023, 3, 1)) }
         it { expect(model.ends_at).to eq(Time.zone.local(2023, 3, 10).end_of_day) }
       end
-  
+
       context 'with a day as a range' do
         let(:range) { Time.zone.local(2023, 3, 1)..Time.zone.local(2023, 3, 1).end_of_day }
-  
+
         before { model.date_range = '2023-03-01' }
-  
+
         it { expect(model.date_range).to eq(range) }
         it { expect(model.starts_at).to eq(Time.zone.local(2023, 3, 1)) }
         it { expect(model.ends_at).to eq(Time.zone.local(2023, 3, 1).end_of_day) }
       end
-  
+
       context 'with date range missing' do
         before { model.date_range = '' }
-  
+
         it { expect(model.date_range).to be_nil }
         it { expect(model.starts_at).to be_nil }
         it { expect(model.ends_at).to be_nil }
@@ -111,7 +111,9 @@ RSpec.describe Bali::Concerns::DateRangeAttribute do
           model.ends_at = 1.week.from_now.end_of_day
         end
 
-        it { expect(model.date_range).to eq(Time.zone.now.beginning_of_day..1.week.from_now.end_of_day) }
+        it {
+          expect(model.date_range).to eq(Time.zone.now.beginning_of_day..1.week.from_now.end_of_day)
+        }
       end
     end
   end
