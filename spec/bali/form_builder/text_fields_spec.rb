76b5eef359b78580a312a0b9e2a0f1f1a67005f5
Miguel Frias <31300198+MiguelFrias97@users.noreply.github.com>
2023-08-09 11:54:41 -0700

    added test

--- spec/bali/form_builder/text_fields_spec.rb
+++ spec/bali/form_builder/text_fields_spec.rb
@@ -6,7 +6,9 @@ RSpec.describe Bali::FormBuilder, type: :form_builder do
   include_context 'form builder'
 
   describe '#text_field_group' do
-    let(:text_field_group) { builder.text_field_group(:name) }
+    let(:text_field_group) { builder.text_field_group(:name, **@options) }
+
+    before { @options = {} }
 
     it 'render a label an input within a wrapper' do
       expect(text_field_group).to have_css 'div.field.field-group-wrapper-component'
@@ -18,6 +20,16 @@ RSpec.describe Bali::FormBuilder, type: :form_builder do
 
     it 'renders an input' do
       expect(text_field_group).to have_css 'input#movie_name[name="movie[name]"]'
+    end
+
+    context 'with addons' do
+      before { @options = { addon_right: '<button>Search</button>'.html_safe } }
+
+      it 'renders a label and input with addons' do 
+        expect(text_field_group).to have_css 'label[for="movie_name"]', text: 'Name'
+        expect(text_field_group).to have_css 'input#movie_name[name="movie[name]"]'
+        expect(text_field_group).to have_css 'div.field.has-addons', text: 'Search'
+      end
     end
   end
 
