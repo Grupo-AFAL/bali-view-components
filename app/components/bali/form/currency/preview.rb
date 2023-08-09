439044f918144c850360457adfe579eda386d695
Miguel Frias <31300198+MiguelFrias97@users.noreply.github.com>
2023-08-09 12:00:45 -0700

    Custom class label currency field group (#365)
    * Fixed visual issue
    
    * Added custom class to label in the currency_field_group when it renders a tooltip
    
    * Fixed issue with addons on inputs
    
    * Fixed rubocop
    
    * Fixed issue
    
    * Removed outdated test
    
    * Added preview
    
    * Added previews
    
    * added test
    
    * Added test
    
    * Fixed ruboco

---
+++ app/components/bali/form/currency/preview.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Bali
+  module Form
+    module Currency
+      class Preview < ApplicationViewComponentPreview
+        def default
+          render_with_template(
+            template: 'bali/form/currency/previews/default',
+            locals: { model: form_record }
+          )
+        end
+
+        def with_errors
+          form_record.errors.add(:currency, :invalid)
+
+          render_with_template(
+            template: 'bali/form/currency/previews/default',
+            locals: { model: form_record }
+          )
+        end
+      end
+    end
+  end
+end
--- spec/bali/form_builder/text_fields_spec.rb
+++ spec/bali/form_builder/text_fields_spec.rb
@@ -6,7 +6,9 @@ RSpec.describe Bali::FormBuilder, type: :form_builder do
   include_context 'form builder'
 
   describe '#text_field_group' do
-    let(:text_field_group) { builder.text_field_group(:name) }
+    let(:text_field_group) { builder.text_field_group(:name, **@options) }
+
+    before { @options = {} }
 
     it 'render a label an input within a wrapper' do
       expect(text_field_group).to have_css 'div.field.field-group-wrapper-component'
@@ -19,10 +21,22 @@ RSpec.describe Bali::FormBuilder, type: :form_builder do
     it 'renders an input' do
       expect(text_field_group).to have_css 'input#movie_name[name="movie[name]"]'
     end
+
+    context 'with addons' do
+      before { @options = { addon_right: '<button>Search</button>'.html_safe } }
+
+      it 'renders a label and input with addons' do
+        expect(text_field_group).to have_css 'label[for="movie_name"]', text: 'Name'
+        expect(text_field_group).to have_css 'input#movie_name[name="movie[name]"]'
+        expect(text_field_group).to have_css 'div.field.has-addons', text: 'Search'
+      end
+    end
   end
 
   describe '#text_field' do
-    let(:text_field) { builder.text_field(:name) }
+    let(:text_field) { builder.text_field(:name, **@options) }
+
+    before { @options = {} }
 
     it 'renders a div with control class' do
       expect(text_field).to have_css 'div.control'
@@ -30,6 +44,15 @@ RSpec.describe Bali::FormBuilder, type: :form_builder do
 
     it 'renders an input' do
       expect(text_field).to have_css 'input#movie_name[name="movie[name]"]'
+    end
+
+    context 'with addons' do
+      before { @options = { addon_right: '<button>Search</button>'.html_safe } }
+
+      it 'renders and input with addons' do
+        expect(text_field).to have_css 'input#movie_name[name="movie[name]"]'
+        expect(text_field).to have_css 'div.field.has-addons', text: 'Search'
+      end
     end
   end
 end
