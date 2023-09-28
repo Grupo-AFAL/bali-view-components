b2379108c7b113936e6bff1c07f3cc1d219e02ca
Miguel Frias <31300198+MiguelFrias97@users.noreply.github.com>
2023-09-28 11:56:52 -0700

    Unify Chart component data structure with Chart.js data structure

--- app/components/bali/chart/preview.rb
+++ app/components/bali/chart/preview.rb
@@ -7,7 +7,7 @@ module Bali
 
       DATA_FORMAT_2 = {
         labels: ['Wed, 12 Jan 2022', 'Thu, 13 Jan 2022'],
-        data: [{ label: 'Res', values: [10, 5] }, { label: 'Cerdo', values: [20, 10] }]
+        datasets: [{ label: 'Res', data: [10, 5] }, { label: 'Cerdo', data: [20, 10] }]
       }.freeze
 
       def bar(id: 'chart')
@@ -49,14 +49,14 @@ module Bali
       def stacked(id: 'chart')
         render Chart::Component.new(
           data: DATA_FORMAT_2, id: id, type: :bar, legend: true,
-          chart_options: { scales: { x: { stacked: true, }, y_1: { stacked: true } } }
+          options: { scales: { x: { stacked: true, }, y: { stacked: true } } }
         )
       end
 
       def multiple_axis(id: 'chart', type: [:bar, :line], title: 'Title')
         render Chart::Component.new(
-          id: id, title: title, data: DATA_FORMAT_2, type: type, axis: [1, 2], order: [1, 0],
-          chart_options: {
+          id: id, title: title, data: DATA_FORMAT_2, type: type, y_axis_ids: ['y_1', 'y_2'], order: [1, 0],
+          options: {
             scales: {
               y_1: { type: 'linear', position: 'left', title: { display: true, text: 'Axis 1' } },
               y_2: { type: 'linear', position: 'right', title: { display: true, text: 'Axis 2' } }
@@ -66,8 +66,8 @@ module Bali
 
       def customize_axes_and_tooltip(id: 'chart', type: [:bar, :line], title: 'Title')
         render Chart::Component.new(
-          id: id, title: title, data: DATA_FORMAT_2, type: type, axis: [1, 2], order: [1, 0],
-          chart_options: {
+          id: id, title: title, data: DATA_FORMAT_2, type: type, y_axis_ids: ['y_1', 'y_2'], order: [1, 0],
+          options: {
             interaction: { intersect: false, mode: :index },
             plugins: { tooltip: { callbacks: { label: { y_1: { suffix: '%' }, y_2: { prefix: '$' } } } } },
             scales: {
