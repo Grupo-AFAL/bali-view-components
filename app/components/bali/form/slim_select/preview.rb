5455e9dc3dd9f1f4263ef9371b21b3ec0e77e25d
Miguel Frias <31300198+MiguelFrias97@users.noreply.github.com>
2024-01-31 14:41:24 -0800

    Added addable

--- app/components/bali/form/slim_select/preview.rb
+++ app/components/bali/form/slim_select/preview.rb
@@ -4,33 +4,32 @@ module Bali
   module Form
     module SlimSelect
       class Preview < ApplicationViewComponentPreview
-        def default
-          options = [
-            ['Option 1', 1],
-            ['Option 2', 2],
-            ['Option 3', 3],
-            ['Option 4', 4],
-            ['Option 5', 5]
-          ]
+        OPTIONS = [
+          ['Option 1', 1],
+          ['Option 2', 2],
+          ['Option 3', 3],
+          ['Option 4', 4],
+          ['Option 5', 5]
+        ].freeze
 
+        def default
           render_with_template(
             template: 'bali/form/slim_select/previews/default',
-            locals: { model: form_record, options: options }
+            locals: { model: form_record, options: OPTIONS }
+          )
+        end
+
+        def addable
+          render_with_template(
+            template: 'bali/form/slim_select/previews/addable',
+            locals: { model: form_record, options: OPTIONS }
           )
         end
 
         def remote
-          options = [
-            ['Option 1', 1],
-            ['Option 2', 2],
-            ['Option 3', 3],
-            ['Option 4', 4],
-            ['Option 5', 5]
-          ]
-
           render_with_template(
             template: 'bali/form/slim_select/previews/remote',
-            locals: { model: form_record, options: options }
+            locals: { model: form_record, options: OPTIONS }
           )
         end
       end
