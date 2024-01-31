cd5a67349f59876d72b095a80319a4abbe6504ee
Miguel Frias <31300198+MiguelFrias97@users.noreply.github.com>
2024-01-31 10:28:41 -0800

    Drowngrade lowlight

--- app/components/bali/rich_text_editor/javascript/lowlight.js
+++ app/components/bali/rich_text_editor/javascript/lowlight.js
@@ -1,4 +1,4 @@
-import { createLowlight } from 'lowlight'
+import { lowlight } from 'lowlight/lib/core.js'
 import css from 'highlight.js/lib/languages/css'
 import javascript from 'highlight.js/lib/languages/javascript'
 import json from 'highlight.js/lib/languages/json'
@@ -7,8 +7,6 @@ import scss from 'highlight.js/lib/languages/scss'
 import sql from 'highlight.js/lib/languages/sql'
 import xml from 'highlight.js/lib/languages/xml'
 import yaml from 'highlight.js/lib/languages/yaml'
-
-const lowlight = createLowlight()
 
 lowlight.registerLanguage('css', css)
 lowlight.registerLanguage('javascript', javascript)
