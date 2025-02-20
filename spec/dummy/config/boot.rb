14e49f690d1c3d9952d919acf63196ccce11a3dd
Miguel Frias <31300198+MiguelFrias97@users.noreply.github.com>
2025-02-20 14:40:44 -0800

    Restore strings

--- spec/dummy/config/boot.rb
+++ spec/dummy/config/boot.rb
@@ -1,4 +1,6 @@
-ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../../../Gemfile", __dir__)
+# frozen_string_literal: true
 
-require "bundler/setup"  if File.exist?(ENV['BUNDLE_GEMFILE'])
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path("../../../Gemfile", __dir__)
+
+require 'bundler/setup'  if File.exist?(ENV['BUNDLE_GEMFILE'])
 $LOAD_PATH.unshift File.expand_path('../../../lib', __dir__)
