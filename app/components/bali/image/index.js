19878fe317f9854a9ad47736f31b4e3ef30fdac9
Miguel Frias <31300198+MiguelFrias97@users.noreply.github.com>
2025-10-03 12:43:19 -0700

    Remove comments

--- app/components/bali/image/index.js
+++ app/components/bali/image/index.js
@@ -1,17 +1,5 @@
 import { Controller } from '@hotwired/stimulus'
 
-/*
-  Avatar Controller
-  Replaces image from img tag with the target output so that a preview of the image can be seen
-
-  It expects the following structure:
-
-  <div data-controller"avatar">
-    <img data-avatar-target"output" src="[some url]">
-    <input data-avatar-target="input" data-action="change->avatar#showImage" />
-  </div>
-
-*/
 export class ImageFieldController extends Controller {
   static targets = ['output', 'input', 'placeholder']
 
