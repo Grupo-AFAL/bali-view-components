ef92c3e25371103fec7b248db5ed12ee6428e34b
Miguel Frias <31300198+MiguelFrias97@users.noreply.github.com>
2025-10-31 15:25:42 -0700

    Fixed queryparmas

--- app/components/bali/utils/url.rb
+++ app/components/bali/utils/url.rb
@@ -11,10 +11,17 @@ module Bali
         uri = URI(url)
         query_params = CGI.parse(uri.query.to_s).merge(values)
         query_params.each do |key, value|
-          query_params[key] = value.first if !key.ends_with?('[]') && value.is_a?(Array)
+          query_params[key] = value.first if !array_query_params?(key) && value.is_a?(Array)
         end
         uri.query = query_params.to_query
         uri.to_s
+      end
+
+      private
+
+      def array_query_params?(query_param_name)
+        query_param_name.ends_with?('[]') ||
+          %w[_in _not_in].any? { |predicate| query_param_name.ends_with?(predicate) }
       end
     end
   end
